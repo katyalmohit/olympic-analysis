25e6065f11f9d07cc0382da8555c027ce59bc9f6
Mohit <128162892+katyalmohit@users.noreply.github.com>
2024-04-28 00:32:31 +0530

    Display dataframe in web-app

--- main/app.py
+++ main/app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+import preprocessor
+
+df = preprocessor.preprocess()
 
 st.sidebar.radio(
     "Select an option",
     ("Medal Tally", "Overall Analysis", "Country-wise Analysis", "Athlete wise Analysis")
 )
+
+st.dataframe(df)
